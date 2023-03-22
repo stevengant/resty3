92e6624f61f33fe701108a216c8835cfd4307299
steven gant <sgant39@gmail.com>
2023-03-22 23:30:04 +0000

    hopefully fixing main

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import './App.scss';
@@ -12,52 +12,45 @@ import Footer from './Components/Footer';
 import Form from './Components/Form';
 import Results from './Components/Results';
 
-// There's no Axios on line 6 ???
-class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: null,
-      requestParams: {},
-    };
-  }
-
-  callApi = (requestParams) => {
-    // mock output
-    const data = {
-      count: 2,
-      results: [
-        {name: 'fake thing 1', url: 'http://fakethings.com/1'},
-        {name: 'fake thing 2', url: 'http://fakethings.com/2'},
-      ],
-    };
-    this.setState({data, requestParams});
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <Header />
-        <div>Request Method: {this.state.requestParams.method}</div>
-        <div>URL: {this.state.requestParams.url}</div>
-        <Form handleApiCall={this.callApi} />
-        <Results data={this.state.data} />
-        <Footer />
-      </React.Fragment>
-    );
-  }
-
-
 function App() {
 
   const [data, setData] = useState(null);
   const [requestParams, setRequestParams] = useState({});
   const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    if (requestParams.url) {
+      callApi(requestParams);
+    }
+  }, [requestParams]);
+
+  const requestParamsUpdate = (requestParams) => {
+    setRequestParams(requestParams);
+  };
+
   const callApi = async(requestParams) => {
+    const { method, url, body } = requestParams;
+
     setLoading(true);
     try {
+      if (!url) {
+        alert("Please enter a valid URL");
+
+      } else {
+        const res = await axios({
+          method,
+          url,
+          data: body,
+        });
+
+        setData({
+          headers: res.headers,
+          body: res.data,
+          status: res.status,
+        });
+
+        setLoading(false);
+      }
       const response = await axios.get(requestParams.url);
       console.log('Response Data: ', response.data);
       const {id, name, abilities} = response.data;
@@ -79,12 +72,11 @@ function App() {
       <Header />
       <div>Request Method: {requestParams.method}</div>
       <div>URL: {requestParams.url}</div>
-      <Form handleApiCall={callApi} />
+      <Form handleApiCall={callApi} requestParamsUpdate={requestParamsUpdate} />
       <Results data={data} loading={loading} />
       <Footer />
     </>
   );
-
 }
 
 export default App;
