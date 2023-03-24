2c63f773aa516cef8a800726abe0b527a9729580
steven gant <sgant39@gmail.com>
2023-03-24 02:56:07 +0000

    attempt made...not done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useReducer } from 'react';
 import axios from 'axios';
 
 import './App.scss';
@@ -13,34 +13,57 @@ import Form from './Components/Form';
 import Results from './Components/Results';
 
 function App() {
+  const initialState = {
+    requestParams: {},
+    data: null,
+    loading: false,
+  }
+  // const [data, setData] = useState(null);
+  // const [requestParams, setRequestParams] = useState({});
+  // const [loading, setLoading] = useState(false);
 
-  const [data, setData] = useState(null);
-  const [requestParams, setRequestParams] = useState({});
-  const [loading, setLoading] = useState(false);
+  const reqReducer = (state = initialState, action) => {
+    switch(action.type) {
+
+      case "UPDATE_URL":
+        return { ...state, url: action.payload};
+        
+      case "UPDATE_METHOD":
+        return { ...state, method: action.payload};
+
+      case "UPDATE_REQUEST_PARAMS":
+        return { ...state, requestParams: action.payload };
+      
+      case "UPDATE_DATA":
+        return { ...state, data: action.payload };
+
+      case "UPDATE_LOADING":
+        return { ...state, loading: action.payload };
+
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reqReducer, initialState);
+
+  const { requestParams, data, loading } = state
 
   useEffect(() => {
-    const getData = async () => {
-      if (requestParams.method && requestParams.url) {
-        setLoading(true);
-
-        const response = await axios(requestParams);
-        const data = response.data;
-        setData(data);
-        setLoading(false);
-      }
+    if(requestParams.url) {
+      dispatch({ type: "UPDATE_LOADING", payload: true });
+      callApi(requestParams);
     }
-
-    getData();
 
   }, [requestParams]);
 
   const requestParamsUpdate = (requestParams) => {
-    setRequestParams(requestParams);
+    dispatch({ type: "UPDATE_REQUEST_PARAMS", payload: requestParams });
+    // dispatch({ type: "UPDATE_HISTORY", payload: requestParams });
   };
 
   const callApi = async (requestParams) => {
     const { method, url, body } = requestParams;
-
 
     try {
       if (!url) {
@@ -53,35 +76,59 @@ function App() {
           data: body,
         });
 
-        setData({
-          headers: res.headers,
-          body: res.data,
-          status: res.status,
+        dispatch({
+          type: "UPDATE_DATA",
+          payload: {
+            headers: res.headers,
+            body: res.data,
+            status: res.status,
+          },
+        });
+
+        dispatch({ 
+          type: "UPDATE_URL", 
+          payload: requestParams 
+        });
+
+        dispatch({ 
+          type: "UPDATE_METHOD", 
+          payload: requestParams 
+        });
+
+        dispatch({ 
+          type: "UPDATE_LOADING", 
+          payload: false 
         });
 
 
       }
       const response = await axios.get(requestParams.url);
       console.log('Response Data: ', response.data);
-      const { id, name, abilities } = response.data;
-      const pokeData = { id, name, abilities };
-      setData(pokeData);
-      setRequestParams(requestParams);
+      // const { id, name, abilities } = response.data;
+      // const pokeData = { id, name, abilities };
+      // setData(pokeData);
+      // setRequestParams(requestParams);
       // console.log('Pokemon Data: ', pokeData);
 
     } catch (error) {
-      console.log(error.message);
+      dispatch({
+        type: "UPDATE_DATA",
+        payload: {
+          headers: error.res.headers,
+          body: error.res.data,
+          status: error.res.status,
+        },
+      });
 
+      dispatch({ type: "UPDATE_LOADING", payload: false });
     }
-
-    setLoading(false);
   };
 
   return (
     <>
       <Header />
-      <div>Request Method: {requestParams.method}</div>
-      <div>URL: {requestParams.url}</div>
+      {/* <div>Request Method: {requestParams.method}</div>
+      <div>URL: {requestParams.url}</div> */}
       <Form handleApiCall={callApi} requestParamsUpdate={requestParamsUpdate} />
       <Results data={data} loading={loading} />
       <Footer />
